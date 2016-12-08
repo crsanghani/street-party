425bf05f67a4af1a12c17b7b923c86cb87c2455b
Amaal Ali <amq.ali@gmail.com>
2016-12-08 12:44:40 +0000

    Define Parties database and Party schema.
    - Defined Parties database in Mongo
    - Party schema created
    - Party schema attached to the Parties database.
    -/-

--- collections/StreetParty.js
+++ collections/StreetParty.js
@@ -1,2 +1,42 @@
+Parties = new Mongo.Collection('parties');
 
-// dummy
+Party = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Party Name"
+  },
+  streetName: {
+    type: String,
+    label: "Street Name"
+  },
+  date: {
+    type: Date,
+    label: "Party Date"
+  },
+  time: {
+    type: String,
+    label: "Party Time"
+  },
+  createdBy: {
+    type: String,
+    label: "Created By",
+    autoValue: function() {
+      return this.userId;
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  createAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+      return new Date();
+    },
+    autoform: {
+      type: "hidden"
+    }
+  }
+});
+
+Parties.attachSchema(Party);
