374c71d5df50258b65bb5a04cdb086bfe7757f2a
Amaal Ali <amq.ali@gmail.com>
2016-12-09 16:53:01 +0000

    Calender displays events, allows editing of events.
    Working:
    - Add events
    - Move events to edit date
    
    Broken:
    - Delete displays modals, error when clicked
    -/-

--- client/events/Events.js
+++ client/events/Events.js
@@ -24,6 +24,33 @@ Template.Events.onRendered( () => {
       element.find( '.fc-content' ).html(
         `<h4>${ event.title }</h4>`
       );
+    },
+    eventDrop( event, delta, revert ) {
+      let date = event.start.format();
+      if ( !isPast( date ) ) {
+        let update = {
+          _id: event._id,
+          start: date,
+          end: date
+        };
+
+        Meteor.call( 'editEvent', update, ( error ) => {
+          if ( error ) {
+            Bert.alert( error.reason, 'danger' );
+          }
+        });
+      } else {
+        revert();
+        Bert.alert( 'Sorry, you can\'t move items to the past!', 'danger' );
+      }
+    },
+    dayClick(date) {
+      Session.set('eventModal', {type: 'add', date: date.format()});
+      $('#add-edit-event-modal').modal('show');
+    },
+    eventClick(event) {
+      Session.set('eventModal', {type: 'edit', edit: event._id});
+      $('#add-edit-event-modal').modal('show');
     }
   });
 
