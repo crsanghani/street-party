d3393ad367d1386229910ac4d289105174518f6b
Tom Nunn <t@tomnunn.uk>
2016-12-09 13:08:01 +0000

    add feature test for Parties, attempting to mock db for Parties data

--- features/featureTests/PartiesSpec.test.js
+++ features/featureTests/PartiesSpec.test.js
@@ -1,7 +1,28 @@
+import { StubCollections } from 'meteor/hwillson:stub-collections';
+import { Parties } from '../../client/parties/Parties.js';
+
+StubCollections.stub(Parties);
+
+Parties.insert({ name: 'Party Name',
+               streetName: 'Street Name',
+               date: '2016-12-08',
+               time: '10:00pm'
+            });
+
+// Restore the `Parties` collection
+StubCollections.restore();
+
 describe("Parties", function () {
 
-  it("should display the page title", function () {
-    expect().to.equal("Parties");
+  beforeEach(() => {
+    browser.url('http://localhost:3000/parties');
   });
 
+  it("should display the page title", function () {
+    expect(browser.getText(".main-layout h1")).to.equal("Parties");
+  });
+
+  it("should be able to see a party", function() {
+    expect(browser.getText(".party")).to.include("Party Name");
+  });
 });
