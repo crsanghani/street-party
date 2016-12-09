2d844b85661aaea8da10b46fc1bb8b2512eafc8e
Tom Nunn <t@tomnunn.uk>
2016-12-09 12:28:10 +0000

    add feature test for task form

--- features/featureTests/TasksSpec.test.js
+++ features/featureTests/TasksSpec.test.js
@@ -1,18 +1,24 @@
 // import { getBrowser, cleanDatabase } from './testHelpers';
-//
-//
-//
-// describe("Tasks", function () {
-//   it("shows a list of task", function () {
-//     browser.url('http://localhost:3000/tasks');
-//     expect(browser.getTitle()).to.equal("street-party");
-//   });
-// });
+
 var chai = require('chai');
 
 describe("Tasks", function () {
-  it("has 'description' in its table", function () {
+
+  beforeEach(() => {
     browser.url('http://localhost:3000/tasks');
+  });
+
+  it("has 'description' in its table", function () {
     expect(browser.getText("label")).to.include('Description');
   });
-});
+
+//   describe("Add Tasks", function() {
+//     it("should be able to add a task", function() {
+//       browser.setValue("[name=name]", "This is a Task")
+//              .setValue("[name=desc]", "This is the Tasks Description")
+//              .setValue("[name=deadline]", Date.now())
+//              .submitForm("#insertTaskForm");
+//       expect(browser.getText(".tasks h3")).to.include("This is a Task");
+//     });
+//   });
+// });
