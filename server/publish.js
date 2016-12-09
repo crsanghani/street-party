767580ef5d322c6418cd77ff2c2fec9809d515e9
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-12-09 16:54:38 +0100

    1) Fixed merge conflict

--- server/publish.js
+++ server/publish.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 Meteor.publish('tasks', function() {
     return Tasks.find();
 });
-=======
+
 Meteor.publish('parties', function() {
-  return Parties.find();
+    return Parties.find();
 });
->>>>>>> 08e6677b91fd94de66ea8ff70e32caddfed9bb75
