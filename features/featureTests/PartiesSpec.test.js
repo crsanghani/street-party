8b10cf53a23814a1952907ac747e8b3fd2e53879
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-12-09 14:14:22 +0100

    change commit

---
+++ features/featureTests/PartiesSpec.test.js
@@ -0,0 +1,7 @@
+describe("Parties", function () {
+
+  it("should display the page title", function () {
+    expect().to.equal("Parties");
+  });
+
+});
--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 Meteor.publish('tasks', function() {
     return Tasks.find();
 });
+=======
+Meteor.publish('parties', function() {
+  return Parties.find();
+});
+>>>>>>> 08e6677b91fd94de66ea8ff70e32caddfed9bb75
