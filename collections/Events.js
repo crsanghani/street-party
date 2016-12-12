39204f7f20d94272de4102f9d9bb7829d4064ca3
Amaal Ali <amq.ali@gmail.com>
2016-12-12 16:20:50 +0000

    Define schema for calendar events.

--- collections/Events.js
+++ collections/Events.js
@@ -1,4 +1,41 @@
 CalEvents = new Mongo.Collection('calevents');
+
+let TasksSchema = new SimpleSchema({
+  'title': {
+    type:   String,
+    label:  "The name of the task."
+  },
+  'start': {
+    type:   String,
+    label:  "When this task is due."
+  },
+  'createdBy': {
+    type:   String,
+    label:  "Administrator",
+    autoValue: function() {
+      return this.userId;
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  // 'assignee': {
+  //   type:   String,
+  //   label:  "Assign this task.",
+  //   autoValue: function() {
+  //     return null;
+  //   }
+  // },
+  // 'completed': {
+  //   type:   Boolean,
+  //   label:  "Has this task been completed?",
+  //   autoValue: false
+  // }
+});
+
+CalEvents.attachSchema( TasksSchema );
+
+
 // Events = new Mongo.Collection( 'events' );
 
 // Events.allow({
