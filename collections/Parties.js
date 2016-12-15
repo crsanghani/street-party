5ef4f16bd869b483040bdb3b102a0c8df3b35450
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-12-15 11:44:34 +0100

    Host name accesible in Party document

--- collections/Parties.js
+++ collections/Parties.js
@@ -1,67 +1,52 @@
 Parties = new Mongo.Collection('parties');
 
-// Host = new SimpleSchema({
-//   firstName: {
-//     type: String,
-//     autoValue: function() {
-//       return Meteor.user().profile.firstName;
-//     }
-//   },
-//   lastName: {
-//     type: String,
-//     autoValue: function() {
-//       return Meteor.user().profile.lastName;
-//     }
-//   }
-// });
-
 Party = new SimpleSchema({
-  name: {
-    type: String,
-    label: "Party Name"
-  },
-  streetName: {
-    type: String,
-    label: "Street Name"
-  },
-  date: {
-    type: Date,
-    label: "Party Date"
-  },
-  time: {
-    type: String,
-    label: "Party Time"
-  },
-  createdBy: {
-    type: String,
-    label: "Created By",
-    autoValue: function() {
-      return this.userId;
+    name: {
+        type: String,
+        label: "Party Name"
     },
-    autoform: {
-      type: "hidden"
-    }
-  },
-  host: {
-    type: String,
-    label: "Hosted By",
-    autoValue: function() {
-      return `${Meteor.user().profile.firstName} ${Meteor.user().profile.lastName}`;
+    streetName: {
+        type: String,
+        label: "Street Name"
     },
-    autoform: {
-      type: "hidden"
-    }
-  },
-  createAt: {
-    type: Date,
-    label: "Created At",
-    autoValue: function() {
-      return new Date();
+    date: {
+        type: Date,
+        label: "Party Date"
     },
-    autoform: {
-      type: "hidden"
+    time: {
+        type: String,
+        label: "Party Time"
+    },
+    createdBy: {
+        type: String,
+        label: "Created By",
+        autoValue: function() {
+            return this.userId;
+        },
+        autoform: {
+            type: "hidden"
+        }
+    },
+    host: {
+        type: String,
+        label: "Hosted By",
+        autoValue: function() {
+            return `${Meteor.user().profile.firstName} ${Meteor.user().profile.lastName}`;
+        },
+        autoform: {
+            type: "hidden"
+        }
+    },
+    createAt: {
+        type: Date,
+        label: "Created At",
+        autoValue: function() {
+            return new Date();
+        },
+        autoform: {
+            type: "hidden"
+        }
     }
-  }
 });
 
 Parties.attachSchema(Party);
